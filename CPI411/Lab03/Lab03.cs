7939c4c02bee6841b57e31bc2d482e91c51e3a49
Nick Hildebrant <nickhildebrant09@gmail.com>
2023-01-26 10:48:14 -0700

    Rendering a Bunny

---
+++ CPI411/Lab03/Lab03.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Lab03
+{
+    public class Lab03 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        Model model;
+
+        Matrix world;
+        Matrix view;
+        Matrix projection;
+
+        float angle;
+        float distance = 15f;
+
+        Effect effect;
+
+        public Lab03()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            _graphics.GraphicsProfile = GraphicsProfile.HiDef;
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            effect = Content.Load<Effect>("SimpleTexture");
+            model = Content.Load<Model>("bunny");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Left))
+            {
+                angle += 0.1f;
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Right))
+            {
+                angle -= 0.1f;
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Up))
+            {
+                distance += 0.05f;
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Down))
+            {
+                distance -= 0.05f;
+            }
+
+            Vector3 cameraPosition = distance * new Vector3((float)System.Math.Sin(angle), 0, (float)System.Math.Cos(angle));
+
+            world = Matrix.Identity;
+            view = Matrix.CreateLookAt(cameraPosition, new Vector3(), new Vector3(0, 1, 0));
+            projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(90), GraphicsDevice.Viewport.AspectRatio, 0.1f, 100);
+
+            effect.Parameters["World"].SetValue(world);
+            effect.Parameters["View"].SetValue(view);
+            effect.Parameters["Projection"].SetValue(projection);
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            model.Draw(world, view, projection);
+
+            base.Draw(gameTime);
+        }
+    }
+}
