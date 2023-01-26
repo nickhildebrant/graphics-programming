9e99df20c0a97524fc7c9bdbd577f6c4a62924d5
Nick Hildebrant <nickhildebrant09@gmail.com>
2023-01-26 11:47:16 -0700

    Added Mouse Rotation

---
+++ CPI411/Lab03/Content/SimpleShading.fx
@@ -0,0 +1,46 @@
+﻿float4x4  World;
+float4x4  View;
+float4x4  Projection;
+float4x4  WorldInverseTranspose;
+float4    AmbientColor;
+float     AmbientIntensity;
+float3    DiffuseLightDirection;
+float4    DiffuseColor;
+float     DiffuseIntensity;
+
+struct VertexInput {
+	float4 Position: POSITION;
+	float4 Normal: NORMAL;
+};
+
+struct VertexOutput {
+	float4 Position: POSITION;
+	float4 Color: COLOR;
+};
+
+VertexOutput MyVertexShader(VertexInput input) : POSITION
+{
+	VertexOutput output;
+	float4 worldPos = mul(input.Position, World);
+	float4 viewPos = mul(worldPos, View);
+	output.Position = mul(viewPos, Projection);
+
+	float4 normal = mul(input.Normal, WorldInverseTranspose);
+	float lightIntensity = dot(normal, DiffuseLightDirection);
+	output.Color = saturate(DiffuseColor * DiffuseIntensity * lightIntensity);
+	return output;
+}
+
+float4 MyPixelShader(VertexOutput input) : COLOR
+{
+	return saturate(input.Color + AmbientColor * AmbientIntensity);
+}
+
+technique MyTechnique
+{
+	pass Pass1
+	{
+		VertexShader = compile vs_4_0 MyVertexShader();
+		PixelShader = compile ps_4_0 MyPixelShader();
+	}
+}
--- CPI411/Lab03/Lab03.cs
+++ CPI411/Lab03/Lab03.cs
@@ -15,10 +15,14 @@ namespace Lab03
         Matrix view;
         Matrix projection;
 
-        float angle;
-        float distance = 15f;
+        float angle, angle2;
+        float distance = 1f;
 
         Effect effect;
+
+        SpriteFont font;
+
+        MouseState previousMouseState;
 
         public Lab03()
         {
@@ -39,8 +43,9 @@ namespace Lab03
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            effect = Content.Load<Effect>("SimpleTexture");
+            effect = Content.Load<Effect>("SimpleShading");
             model = Content.Load<Model>("bunny");
+            //font = Content.Load<SpriteFont>("font");
         }
 
         protected override void Update(GameTime gameTime)
@@ -48,35 +53,23 @@ namespace Lab03
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            if (Keyboard.GetState().IsKeyDown(Keys.Left))
+            if (Mouse.GetState().LeftButton == ButtonState.Pressed)
             {
-                angle += 0.1f;
+                angle += 0.1f * (Mouse.GetState().X - previousMouseState.X);
+                angle2 += 0.1f * (Mouse.GetState().Y - previousMouseState.Y);
             }
 
-            if (Keyboard.GetState().IsKeyDown(Keys.Right))
+            if (Mouse.GetState().RightButton == ButtonState.Pressed)
             {
-                angle -= 0.1f;
+                distance += 0.1f * (Mouse.GetState().Y - previousMouseState.Y);
             }
 
-            if (Keyboard.GetState().IsKeyDown(Keys.Up))
-            {
-                distance += 0.05f;
-            }
+            Vector3 camera = Vector3.Transform(distance * new Vector3(0, 0, 20), Matrix.CreateRotationX(angle2) * Matrix.CreateRotationY(angle));
+            view = Matrix.CreateLookAt(camera, Vector3.Zero, Vector3.UnitY);
 
-            if (Keyboard.GetState().IsKeyDown(Keys.Down))
-            {
-                distance -= 0.05f;
-            }
-
-            Vector3 cameraPosition = distance * new Vector3((float)System.Math.Sin(angle), 0, (float)System.Math.Cos(angle));
-
-            world = Matrix.Identity;
-            view = Matrix.CreateLookAt(cameraPosition, new Vector3(), new Vector3(0, 1, 0));
             projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(90), GraphicsDevice.Viewport.AspectRatio, 0.1f, 100);
 
-            effect.Parameters["World"].SetValue(world);
-            effect.Parameters["View"].SetValue(view);
-            effect.Parameters["Projection"].SetValue(projection);
+            previousMouseState = Mouse.GetState();
 
             base.Update(gameTime);
         }
@@ -84,8 +77,40 @@ namespace Lab03
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.DepthStencilState = new DepthStencilState();
 
-            model.Draw(world, view, projection);
+            _spriteBatch.Begin();
+            //_spriteBatch.DrawString(font, "angle:" + angle, Vector2.UnitX + Vector2.UnitY * 12, Color.White);
+            _spriteBatch.End();
+
+            effect.CurrentTechnique = effect.Techniques[0];
+            foreach (EffectPass pass in effect.CurrentTechnique.Passes)
+            {
+                foreach (ModelMesh mesh in model.Meshes)
+                {
+
+                    effect.Parameters["World"].SetValue(mesh.ParentBone.Transform);
+                    effect.Parameters["View"].SetValue(view);
+                    effect.Parameters["Projection"].SetValue(projection);
+                    effect.Parameters["AmbientColor"].SetValue(Color.Black.ToVector3());
+                    effect.Parameters["AmbientIntensity"].SetValue(2f);
+                    effect.Parameters["DiffuseColor"].SetValue(new Vector3(0.75f, 0.75f, 0.75f));
+                    effect.Parameters["DiffuseLightDirection"].SetValue(Vector3.Up);
+                    effect.Parameters["DiffuseIntensity"].SetValue(1f);
+
+                    Matrix worldInverseTranspose = Matrix.Transpose(Matrix.Invert(mesh.ParentBone.Transform));
+                    effect.Parameters["WorldInverseTranspose"].SetValue(worldInverseTranspose);
+                    pass.Apply();
+
+                    foreach (ModelMeshPart part in mesh.MeshParts)
+                    {
+                        GraphicsDevice.SetVertexBuffer(part.VertexBuffer);
+                        GraphicsDevice.Indices = part.IndexBuffer;
+
+                        GraphicsDevice.DrawIndexedPrimitives(PrimitiveType.TriangleList, part.VertexOffset, 0, part.NumVertices, part.StartIndex, part.PrimitiveCount);
+                    }
+                }
+            }
 
             base.Draw(gameTime);
         }
