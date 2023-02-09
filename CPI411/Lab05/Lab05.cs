3b0b7f8bf2192e3a6bfecf1c31fb510c2b74e03b
Nick Hildebrant <nickhildebrant09@gmail.com>
2023-02-09 11:42:29 -0700

    Added the Skybox

--- CPI411/Lab05/Lab05.cs
+++ CPI411/Lab05/Lab05.cs
@@ -2,12 +2,38 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
+using CPI411.SimpleEngine;
+
 namespace Lab05
 {
     public class Lab05 : Game
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+
+        Skybox skybox;
+        string[] skyboxTextures = 
+            {
+                "skybox/SunsetPNG2",
+                "skybox/SunsetPNG1",
+                "skybox/SunsetPNG4",
+                "skybox/SunsetPNG3",
+                "skybox/SunsetPNG6",
+                "skybox/SunsetPNG5",
+            };
+
+        Vector3 cameraPosition;
+        Matrix view;
+        Matrix projection;
+
+        float angle, angle2;
+        float distance = 1f;
+
+        Effect effect;
+
+        SpriteFont font;
+
+        MouseState previousMouseState;
 
         public Lab05()
         {
@@ -18,7 +44,7 @@ namespace Lab05
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+            skybox = new Skybox(skyboxTextures, Content, GraphicsDevice);
 
             base.Initialize();
         }
@@ -27,7 +53,6 @@ namespace Lab05
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
         }
 
         protected override void Update(GameTime gameTime)
@@ -35,7 +60,22 @@ namespace Lab05
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
+            if (Mouse.GetState().LeftButton == ButtonState.Pressed)
+            {
+                angle += 0.1f * (Mouse.GetState().X - previousMouseState.X);
+                angle2 += 0.1f * (Mouse.GetState().Y - previousMouseState.Y);
+            }
+
+            if (Mouse.GetState().RightButton == ButtonState.Pressed)
+            {
+                distance += 0.1f * (Mouse.GetState().Y - previousMouseState.Y);
+            }
+
+            cameraPosition = Vector3.Transform(new Vector3(0, 0, 20), Matrix.CreateRotationX(angle2) * Matrix.CreateRotationY(angle));
+            view = Matrix.CreateLookAt(distance * cameraPosition, new Vector3(), Vector3.Transform(Vector3.Up, Matrix.CreateRotationX(angle2) * Matrix.CreateRotationY(angle)));
+            projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(90), GraphicsDevice.Viewport.AspectRatio, 0.1f, 100);
+
+            previousMouseState = Mouse.GetState();
 
             base.Update(gameTime);
         }
@@ -43,8 +83,11 @@ namespace Lab05
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
+            RasterizerState rasterizerState = new RasterizerState();
+            rasterizerState.CullMode = CullMode.None;
+            GraphicsDevice.RasterizerState = rasterizerState;
 
-            // TODO: Add your drawing code here
+            skybox.Draw(view, projection, cameraPosition);
 
             base.Draw(gameTime);
         }
