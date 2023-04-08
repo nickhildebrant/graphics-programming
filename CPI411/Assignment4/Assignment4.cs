fabfc0b138dfc85b5c701a6e3aaea3df9042154e
nickhildebrant <nickhildebrant09@gmail.com>
2023-04-08 14:20:57 -0700

    Added Assignment 4

---
+++ CPI411/Assignment4/Assignment4.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Assignment4
+{
+    public class Assignment4 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        SpriteFont font;
+        Model model;
+        Texture2D texture;
+        Effect effect;
+
+        bool showInfo = true, showHelp = true;
+
+        Matrix world = Matrix.Identity;
+        Matrix view, projection;
+
+        Vector3 cameraPosition;
+        Vector3 cameraTarget;
+
+        Vector3 lightPosition = new Vector3(0, 0, 1);
+        Matrix lightView;
+        Matrix lightProjection = Matrix.CreatePerspectiveFieldOfView(MathHelper.PiOver2, 1f, 1f, 50f);
+
+        float cameraAngleX, cameraAngleY;
+        float lightAngleX, lightAngleY = 30;
+        float distance = 15f;
+
+        MouseState previousMouseState;
+        KeyboardState previousKeyboardState;
+
+        public Assignment4()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            _graphics.GraphicsProfile = GraphicsProfile.HiDef;
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(90), GraphicsDevice.Viewport.AspectRatio, 0.1f, 1000);
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            font = Content.Load<SpriteFont>("font");
+            model = Content.Load<Model>("Plane");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // Info UI + Help UI
+            if (Keyboard.GetState().IsKeyDown(Keys.H) && !previousKeyboardState.IsKeyDown(Keys.H)) { showInfo = !showInfo; }
+            if (Keyboard.GetState().IsKeyDown(Keys.OemQuestion) && !previousKeyboardState.IsKeyDown(Keys.OemQuestion)) { showHelp = !showHelp; }
+
+            // Reset the camera
+            if (Keyboard.GetState().IsKeyDown(Keys.S)) { cameraAngleX = cameraAngleY = lightAngleX = lightAngleY = 0; distance = 15; cameraTarget = Vector3.Zero; }
+
+            // Distance control
+            if (previousMouseState.RightButton == ButtonState.Pressed && Mouse.GetState().RightButton == ButtonState.Pressed)
+            {
+                distance += (Mouse.GetState().X - previousMouseState.X) / 100f;
+            }
+
+            // Camera rotation control
+            if (previousMouseState.LeftButton == ButtonState.Pressed && Mouse.GetState().LeftButton == ButtonState.Pressed)
+            {
+                cameraAngleX += (previousMouseState.X - Mouse.GetState().X);
+                cameraAngleY += (previousMouseState.Y - Mouse.GetState().Y);
+            }
+
+            // Camera translation control
+            if (previousMouseState.MiddleButton == ButtonState.Pressed && Mouse.GetState().MiddleButton == ButtonState.Pressed)
+            {
+                Vector3 ViewRight = Vector3.Transform(Vector3.UnitX, Matrix.CreateRotationX(cameraAngleY) * Matrix.CreateRotationY(cameraAngleX));
+                Vector3 ViewUp = Vector3.Transform(Vector3.UnitY, Matrix.CreateRotationX(cameraAngleY) * Matrix.CreateRotationY(cameraAngleX));
+                cameraTarget -= ViewRight * (Mouse.GetState().X - previousMouseState.X) / 10f;
+                cameraTarget += ViewUp * (Mouse.GetState().Y - previousMouseState.Y) / 10f;
+            }
+
+            cameraPosition = Vector3.Transform(new Vector3(0, 0, distance), Matrix.CreateTranslation(cameraTarget) * Matrix.CreateRotationX(MathHelper.ToRadians(cameraAngleY)) * Matrix.CreateRotationY(MathHelper.ToRadians(cameraAngleX)));
+            view = Matrix.CreateLookAt(cameraPosition, cameraTarget, Vector3.Transform(Vector3.UnitY, Matrix.CreateRotationX(MathHelper.ToRadians(cameraAngleY)) * Matrix.CreateRotationY(MathHelper.ToRadians(cameraAngleX))));
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Up)) lightAngleY += 0.1f;
+            if (Keyboard.GetState().IsKeyDown(Keys.Down)) lightAngleY -= 0.1f;
+            if (Keyboard.GetState().IsKeyDown(Keys.Left)) lightAngleX += 0.1f;
+            if (Keyboard.GetState().IsKeyDown(Keys.Right)) lightAngleX -= 0.1f;
+
+            lightPosition = Vector3.Transform(new Vector3(0, 0, 20), Matrix.CreateRotationX(lightAngleY) * Matrix.CreateRotationY(lightAngleX));
+            lightView = Matrix.CreateLookAt(lightPosition, Vector3.Zero, Vector3.Transform(Vector3.UnitY, Matrix.CreateRotationX(lightAngleY) * Matrix.CreateRotationY(lightAngleX)));
+
+            previousMouseState = Mouse.GetState();
+            previousKeyboardState = Keyboard.GetState();
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.BlendState = BlendState.AlphaBlend;
+            GraphicsDevice.DepthStencilState = new DepthStencilState();
+
+            GraphicsDevice.DepthStencilState = DepthStencilState.DepthRead;
+            GraphicsDevice.RasterizerState = RasterizerState.CullNone;
+
+            model.Draw(world, view, projection);
+
+            base.Draw(gameTime);
+        }
+    }
+}
