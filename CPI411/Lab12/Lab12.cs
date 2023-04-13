6ba0a918d251b961641b4ae8b22d06e8a9dfacf8
Nick Hildebrant <nickhildebrant09@gmail.com>
2023-04-13 10:49:02 -0700

    Added Camera Controls

--- CPI411/Lab12/Lab12.cs
+++ CPI411/Lab12/Lab12.cs
@@ -20,9 +20,19 @@ namespace Lab12
         Matrix view = Matrix.CreateLookAt(new Vector3(0, 0, 20), new Vector3(0, 0, 0), Vector3.UnitY);
         Matrix projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(45), 800f / 600f, 0.1f, 100f);
 
+        Vector3 cameraPosition;
+        Vector3 cameraTarget;
+
+        float cameraAngleX = -30, cameraAngleY = -30;
+        float distance = 15f;
+
+        MouseState previousMouseState;
+        KeyboardState previousKeyboardState;
+
         public Lab12()
         {
             _graphics = new GraphicsDeviceManager(this);
+            _graphics.GraphicsProfile = GraphicsProfile.HiDef;
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
         }
@@ -43,14 +53,42 @@ namespace Lab12
 
             model = Content.Load<Model>("objects");
             depthAndNormalMap = Content.Load<Texture2D>("noise");
-            effect = Content.Load<Effect>("DepthAndNormal");
         }
 
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape)) Exit();
 
-            // TODO: Add your update logic here
+            // Reset the camera
+            if (Keyboard.GetState().IsKeyDown(Keys.S)) { cameraAngleX = cameraAngleY = -30; distance = 15; cameraTarget = Vector3.Zero; }
+
+            // Distance control
+            if (previousMouseState.RightButton == ButtonState.Pressed && Mouse.GetState().RightButton == ButtonState.Pressed)
+            {
+                distance += (Mouse.GetState().X - previousMouseState.X) / 100f;
+            }
+
+            // Camera rotation control
+            if (previousMouseState.LeftButton == ButtonState.Pressed && Mouse.GetState().LeftButton == ButtonState.Pressed)
+            {
+                cameraAngleX += (previousMouseState.X - Mouse.GetState().X);
+                cameraAngleY += (previousMouseState.Y - Mouse.GetState().Y);
+            }
+
+            // Camera translation control
+            if (previousMouseState.MiddleButton == ButtonState.Pressed && Mouse.GetState().MiddleButton == ButtonState.Pressed)
+            {
+                Vector3 ViewRight = Vector3.Transform(Vector3.UnitX, Matrix.CreateRotationX(cameraAngleY) * Matrix.CreateRotationY(cameraAngleX));
+                Vector3 ViewUp = Vector3.Transform(Vector3.UnitY, Matrix.CreateRotationX(cameraAngleY) * Matrix.CreateRotationY(cameraAngleX));
+                cameraTarget -= ViewRight * (Mouse.GetState().X - previousMouseState.X) / 10f;
+                cameraTarget += ViewUp * (Mouse.GetState().Y - previousMouseState.Y) / 10f;
+            }
+
+            cameraPosition = Vector3.Transform(new Vector3(0, 0, distance), Matrix.CreateTranslation(cameraTarget) * Matrix.CreateRotationX(MathHelper.ToRadians(cameraAngleY)) * Matrix.CreateRotationY(MathHelper.ToRadians(cameraAngleX)));
+            view = Matrix.CreateLookAt(cameraPosition, cameraTarget, Vector3.Transform(Vector3.UnitY, Matrix.CreateRotationX(MathHelper.ToRadians(cameraAngleY)) * Matrix.CreateRotationY(MathHelper.ToRadians(cameraAngleX))));
+
+            previousMouseState = Mouse.GetState();
+            previousKeyboardState = Keyboard.GetState();
 
             base.Update(gameTime);
         }
@@ -85,7 +123,7 @@ namespace Lab12
 
         private void DrawDepthAndNormalMap()
         {
-            effect = Content.Load<Effect>("DepthAndNormalMap");
+            effect = Content.Load<Effect>("DepthAndNormal");
             effect.CurrentTechnique = effect.Techniques[0];
             foreach (EffectPass pass in effect.CurrentTechnique.Passes)
             {
