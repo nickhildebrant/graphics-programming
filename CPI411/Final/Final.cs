88b8be3762aa407102c908b1329e375bc4b9b5ef
nickhildebrant <nickhildebrant09@gmail.com>
2023-04-16 12:08:31 -0700

    Revert "Revert "Added Final Project""
    This reverts commit 8c59f8c10f60a8e3d7b3c39f6553ea5cdd5e5b66.

---
+++ CPI411/Final/Final.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Final
+{
+    public class Final : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        SpriteFont font;
+        Model model;
+        Texture2D[] textures;
+        Effect effect;
+
+        bool showInfo = true, showHelp = true;
+
+        Matrix world = Matrix.Identity;
+        Matrix view, projection;
+
+        Vector3 cameraPosition;
+        Vector3 cameraTarget;
+
+        float cameraAngleX = -30, cameraAngleY = -30;
+        float distance = 15f;
+
+        MouseState previousMouseState;
+        KeyboardState previousKeyboardState;
+
+        public Final()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape)) Exit();
+
+            // Info UI + Help UI
+            if (Keyboard.GetState().IsKeyDown(Keys.H) && !previousKeyboardState.IsKeyDown(Keys.H)) { showInfo = !showInfo; }
+            if (Keyboard.GetState().IsKeyDown(Keys.OemQuestion) && !previousKeyboardState.IsKeyDown(Keys.OemQuestion)) { showHelp = !showHelp; }
+
+            // Reset the camera
+            if (Keyboard.GetState().IsKeyDown(Keys.S)) { cameraAngleX = cameraAngleY = -30; distance = 15; cameraTarget = Vector3.Zero; }
+
+            // Distance control
+            if (previousMouseState.RightButton == ButtonState.Pressed && Mouse.GetState().RightButton == ButtonState.Pressed)
+            {
+                distance += (Mouse.GetState().X - previousMouseState.X) / 100f;
+            }
+
+            // Camera rotation control
+            if (previousMouseState.LeftButton == ButtonState.Pressed && Mouse.GetState().LeftButton == ButtonState.Pressed)
+            {
+                cameraAngleX += (previousMouseState.X - Mouse.GetState().X);
+                cameraAngleY += (previousMouseState.Y - Mouse.GetState().Y);
+            }
+
+            // Camera translation control
+            if (previousMouseState.MiddleButton == ButtonState.Pressed && Mouse.GetState().MiddleButton == ButtonState.Pressed)
+            {
+                Vector3 ViewRight = Vector3.Transform(Vector3.UnitX, Matrix.CreateRotationX(cameraAngleY) * Matrix.CreateRotationY(cameraAngleX));
+                Vector3 ViewUp = Vector3.Transform(Vector3.UnitY, Matrix.CreateRotationX(cameraAngleY) * Matrix.CreateRotationY(cameraAngleX));
+                cameraTarget -= ViewRight * (Mouse.GetState().X - previousMouseState.X) / 10f;
+                cameraTarget += ViewUp * (Mouse.GetState().Y - previousMouseState.Y) / 10f;
+            }
+
+            cameraPosition = Vector3.Transform(new Vector3(0, 0, distance), Matrix.CreateTranslation(cameraTarget) * Matrix.CreateRotationX(MathHelper.ToRadians(cameraAngleY)) * Matrix.CreateRotationY(MathHelper.ToRadians(cameraAngleX)));
+            view = Matrix.CreateLookAt(cameraPosition, cameraTarget, Vector3.Transform(Vector3.UnitY, Matrix.CreateRotationX(MathHelper.ToRadians(cameraAngleY)) * Matrix.CreateRotationY(MathHelper.ToRadians(cameraAngleX))));
+
+            previousMouseState = Mouse.GetState();
+            previousKeyboardState = Keyboard.GetState();
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
